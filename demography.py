b2a1f407cbf3c0a2e5b6adb5a5a1c189cc4b85a0
Patricia <patricia.monfa-matas@epitech.eu>
2020-04-29 17:48:40 +0200

    [ADD] retrieve data from file and print countries

--- demography.py
+++ demography.py
@@ -8,6 +8,8 @@
 #                                          #
 ############################################
 
+import csv
+import sys
 
 class Demography():
 
@@ -16,13 +18,51 @@ class Demography():
     """
 
     def __init__(self):
-        self._d = 0
-        self.run()
+        self._country = sys.argv[1:]
+        self._data = []
 
+    def getData(self):
+        """
+        Get data from CSV file
+        """
+
+        with open("./207demography_data.csv", 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=';')
+            for row in csv_reader:
+                self._data.append(row)
+
+    def printCountry(self):
+        """
+        Print country's name
+        """
+        x = 0
+        i = 0
+        
+        while x < len(self._data):
+            if (i == len(self._country)):
+                break
+            if (i < 1):
+                if (self._data[x][1] == self._country[i]):
+                    print("Country = {}".format(self._data[x][0]), end="")
+                    i += 1
+                    x = 0
+                else:
+                    x += 1
+            elif (i >= 1):
+                if (self._data[x][1] == self._country[i]):
+                    print(", {}".format(self._data[x][0]), end="")
+                    i += 1
+                    x = 0
+                else:
+                    x += 1
+        print()
+        
     def run(self):
 
         """
         Run computations and process output printing.
         """
 
+        self.getData()
+        self.printCountry()
         return 0
